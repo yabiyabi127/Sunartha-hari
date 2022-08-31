cc48d9b30235afdbfa5e9ec52641918206274020
hariniawa127 <hariniawadev@gmail.com>
2022-08-31 13:39:50 +0700

    fix

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -7,16 +7,9 @@ import MainLayout from 'layout/MainLayout';
 // render - dashboard
 const DashboardDefault = Loadable(lazy(() => import('pages/dashboard')));
 
-// render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
-
 // render - utilities
-const Typography = Loadable(lazy(() => import('pages/components-overview/Warehouse')));
 const WarehouseDetail = Loadable(lazy(() => import('pages/components-overview/WarehouseDetail')));
 const Warehouse = Loadable(lazy(() => import('pages/components-overview/Warehouse')));
-const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
-const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
-const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
@@ -33,20 +26,8 @@ const MainRoutes = {
             element: <WarehouseDetail />
         },
         {
-            path: 'sample-page',
-            element: <SamplePage />
-        },
-        {
-            path: 'shadow',
-            element: <Shadow />
-        },
-        {
             path: 'warehouse',
             element: <Warehouse />
-        },
-        {
-            path: 'icons/ant',
-            element: <AntIcons />
         }
     ]
 };
